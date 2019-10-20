4c8ebb132bd5ba15bf309db98d746edab66201cc
Tre <amack4693@gmail.com>
2019-10-20 19:55:39 -0400

    modified api in js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const url = 'https://rickandmortyapi.com/api/'
+const url = 'https://rickandmortyapi.com/api/character'
+const h4 = document.querySelector('h4')
 fetch(url)
     .then(res => {
         return res.json()
     })
     .then(res => {
-        console.log('it works!', res)
+        console.log(res.results)
+        let characters = res.results
+        for (i = 0; i < characters.length; i++) {
+            console.log(characters[i])
+            let p =document.createElement('p')
+            h4.innerText=characters[i].name
+            h4.appendChild(p)
+        }
     })
     .catch(err => {
         console.log('uh oh', err)
     })
 
+
+    
     function openTab(evt, tabName) {
+        evt.preventDefault()
         var i, tabcontent, tablinks;
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
