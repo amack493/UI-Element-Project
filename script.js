a2eaa7499830987cc4a04e5e6069ed028fbfbaa8
Tre <amack4693@gmail.com>
2019-10-20 01:22:40 -0400

    added functionality to tabs

--- script.js
+++ script.js
@@ -10,42 +10,16 @@ fetch(url)
         console.log('uh oh', err)
     })
 
-
-
-
-
-
-
-
-
-
-
-
     
-// const tab = document.getElementsByClassName('tabLinks');
-// for (i = 0; i < tabLinks.length; i++) {
-//     tab[i].addEventListeners(click, handleClickEvents);
-//     function handleClickEvents(e) {
-//         console.log('clicked');
-//         console.log(e);
-// }
-
-// }
 
 
 
-// function openTab (e, characters) {
-//     var i, tabContent, tabLinks;
-//     tabContent = document.getElementsByClassName ('tabContent');
-//     for (i = 0; i < tabContent.length; i++){
-//         tabContent[i].style.display = 'none';
-//     }
 
-// tablinks = document.getElementsByClassName('tablinks');
-// for (i = 0; i <  tabLinks.length; i++) {
-//     tabLinks[i].className = tabLinks[i].className.replace('active', " ");
-// }
 
-// document.getElementById(characters).style.display = 'block';
-// e.currentTarget.className += 'active';
-// }
+
+
+
+
+
+
+
